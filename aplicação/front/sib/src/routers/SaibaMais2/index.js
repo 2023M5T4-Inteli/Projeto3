aafe84166de902d1993906daf58f6f31840d8bad
kathlyn diwan <kathlyn.diwan@sou.inteli.edu.br>
2023-03-20 16:59:55 -0300

    Adicionei o código
    adicionei o código que eu fiz para a tela do Saiba Mais, e pretendo mexer nele e alterar ele no formato padrão.

---
+++ aplicação/front/sib/src/routers/SaibaMais2/index.js
@@ -0,0 +1,43 @@
+import "./index.css";
+import Logo from "./img/logo.png";
+import Caixa from "./img/verdinho.png";
+import Escudo from "./img/escudo.png";
+import hour from "./img/hour.png";
+
+export default function App() {
+  return (
+    <div className="logo">
+      <img src={Logo} alt="Logo" />
+      <img src={hour} alt="hour" />
+
+      <div className="caixa">
+        <img className="Caixa" src={Caixa} alt="Caixa" />
+        <img className="Escudo" src={Escudo} alt="Escudo" />
+
+        <p className="texto1">Sobre a Coover</p>
+      </div>
+
+      <div className="caixa2">
+        <p className="texto2">
+          Utilizamos tecnologia para simplificar e agilizar o processo de
+          contratação de seguros.
+        </p>
+      </div>
+
+      <div className="caixa3">
+        <p className="texto3">
+          Empresa de seguros focada em melhorar a usabilidade dos nossos
+          usuários.{" "}
+        </p>
+      </div>
+
+      <div className="caixa4">
+        <p className="texto4">
+          Oferecemos uma ampla gama de seguros. Nossos clientes podem comparar
+          diferentes planos de seguros e escolher o que melhor atende às suas
+          necessidades e orçamento.
+        </p>
+      </div>
+    </div>
+  );
+}
