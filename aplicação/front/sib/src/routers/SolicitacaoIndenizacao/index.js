c1c90fbb3b10b18f63f6635bd1b381cacf35752b
eularibr <larissa.carvalho@sou.inteli.edu.br>
2023-03-21 14:44:04 -0300

    Atualização da página Solicitação Indenização
    Atualização do Css e também padronização.

--- aplicação/front/sib/src/routers/SolicitacaoIndenizacao/index.js
+++ aplicação/front/sib/src/routers/SolicitacaoIndenizacao/index.js
@@ -1,35 +1,35 @@
-import Layout  from "../../componentes/Layout";
+import "./SolicitacaoIndenizacao.css";
+import Layout from "../../componentes/Layout";
 import Logo from '../../Assets/logoCoover.svg'
+import Check from "../../Assets/IconCheck.png";
 
-import check from "../../Assets/IconCheck.png";
-
-export const SolicitacaoIndenizacao = () => { 
-  return (
-    <Layout>
-       <div className="logo">
-        <img src={Logo} alt="Logo" />
-        <div className="check">
-            <img src={check} alt="check" />
-        </div>
-        <div>
-            <p className="texto"> Pedido de indenização enviado com sucesso!</p>
-            <p className="texto1"> Sua solicitação já foi enviada! Agora só falta aguardar pela resposta da seguradora. </p>
-            <p className="texto2">Status da sua solicitação</p>
-            <div className="button">
-            <button className="button1">
-                <span>Em aberto</span>
-                </button>
-            <button className="button2">
-                <span>Abrir minha solicitação</span>
-            </button>
-            <button className="button3">
-                <span>Voltar para menu inicial</span>
-                </button>
+export const SolicitacaoIndenizacao = () => {
+    return (
+        <Layout>
+            <div className="logo">
+                <img src={Logo} alt="Logo" />
+                <div className="check">
+                    <img src={Check} alt="check" />
+                </div>
+                <div>
+                    <p className="texto"> Pedido de indenização enviado com sucesso!</p> 
+                    <p className="texto2"> Sua solicitação já foi enviada! Agora só falta aguardar pela resposta da seguradora. </p>
+                    <p className="texto3">Status da sua solicitação</p>
+                    <div className="button">
+                        <button className="button1">
+                            <span>Em aberto</span>
+                        </button>
+                        <button className="button2">
+                            <span>Abrir minha solicitação</span>
+                        </button>
+                        <button className="button3">
+                            <span>Voltar para menu inicial</span>
+                        </button>
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
-    </Layout>
-  );
+        </Layout>
+    );
 }
 
 export default SolicitacaoIndenizacao;
